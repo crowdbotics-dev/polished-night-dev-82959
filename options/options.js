abad3789e272b1ee39aa5618a8ed13645a468ab7
Crowdbotics <engineering@crowdbotics.com>
2023-05-13 23:09:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "polished-night-dev-82959",
+  url: "https://polished-night-dev-82959.botics.co",
+  api: "https://polished-night-dev-82959.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
